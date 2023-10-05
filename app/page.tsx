b37ac8bc7b746e9796d345e8ca2109137402dda8
AbdelhakMHD <abdelhakmehda@gmail.com>
2023-10-05 19:35:49 +0100

    add Clerk authentication functionality

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1 className="h1-bold">Here we go</h1>
-      <h2 className="h2-bold">Here we go</h2>
-      <h3 className="h3-bold">Here we go</h3>
-    </main>
-  );
-}
