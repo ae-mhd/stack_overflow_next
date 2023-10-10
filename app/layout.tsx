121dffec096afd83d8030b98b78d08e4b65ed38c
AbdelhakMHD <abdelhakmehda@gmail.com>
2023-10-10 12:18:32 +0100

    implement theme Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next/types";
 
 import "./globals.css";
+import { ThemProvider } from "@/context/ThemProvider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -31,19 +32,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: "primary-gradient",
-          footerActionLink: "primary-text-gradient hover:text-primary-500",
-        },
-      }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemProvider>{children}</ThemProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
---
+++ context/ThemProvider.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
+}
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState("");
+  const handleThemeChnage = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.documentElement.classList.add("light");
+    } else {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    }
+  };
+  useEffect(() => {
+    handleThemeChnage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a themeProvider");
+  }
+  return context;
+}
