180ada92e74f9d01da537a137aad86d138630e8e
AbdelhakMHD <abdelhakmehda@gmail.com>
2023-12-07 16:20:25 +0100

    add Left Sidebar Comp

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
-import { Navbar } from "@/components";
+// import { Navbar } from "@/components";
+import LeftSideBar from "@/components/shared/LeftSideBar";
+import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -6,13 +8,12 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        leftbar
+        <LeftSideBar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14 ">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
         rightbar
       </div>
-      Toaster
     </main>
   );
 };
--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,4 @@ import Navbar from "./shared/navbar/Navbar";
 import Theme from "./shared/navbar/Theme";
 import GlobalSearch from "./shared/search/GlobalSearch";
 
-export { Navbar, Theme, GlobalSearch };
+export { Theme, GlobalSearch, Navbar };
